7800d5ee2a73fa1863d655ed844fea92f941ecb0
khaled hossain <md.khaled.eee@gmail.com>
2020-05-31 05:30:29 +0600

    add addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,5 +4,9 @@ module.exports = {
     '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
     '@storybook/addon-links',
+    '@storybook/addon-knobs/register',
+    '@storybook/addon-storysource',
+    '@storybook/addon-viewport/register',
+    '@storybook/addon-backgrounds/register'
   ],
 };
---
+++ .storybook/preview.js
@@ -0,0 +1,9 @@
+import '@storybook/addon-console';
+import { addParameters } from '@storybook/react'; // <- or your storybook framework
+
+addParameters({
+  backgrounds: [
+    { name: 'twitter', value: '#00aced', default: true },
+    { name: 'facebook', value: '#3b5998' },
+  ],
+});
